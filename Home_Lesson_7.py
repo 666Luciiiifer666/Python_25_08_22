6951a4a79e95a0b859aa62798b0d0982897c42f7
nikita <nikita.ylitin@gmail.com>
2022-09-28 23:33:24 +0300

    Home lesson 7

--- Home_Lesson_7.py
+++ Home_Lesson_7.py
@@ -4,7 +4,6 @@ m_int = str(m_int)
 number = "0"
 counted = m_int.count(number)
 
-
 # 2. Дано целое число (int). Определить сколько нулей в конце этого числа. Например для числа 1002000 - три нуля
 value = 1002000
 value = str(value)
@@ -12,23 +11,21 @@ number = 0
 for symbol in value:
     if int(symbol) == 0:
         number = number + 1
-    else: number = 0
-
+    else:
+        number = 0
 
 # 3. Даны списки my_list_1 и my_list_2.
 # Создать список my_result в который вначале поместить
 # элементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.
 
 my_list_1 = "jpjppmp"
-my_list_2 = "okpgks"
+my_list_2 = "okapis"
 my_result = my_list_1[::2] + my_list_2[1::2]
-
 
 # 4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list
 # стоит на последнем месте. Если my_list [1,2,3,4], то new_list [2,3,4,1]
 my_list = "python"
 new_list = my_list[1:] + my_list[:1]
-
 
 # 5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.
 # [1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя! (используйте метод pop)
@@ -59,7 +56,6 @@ l_index = my_str.find(l_limit) + 1
 r_index = my_str.rfind(r_limit)
 sub_str = my_str[l_index: r_index]
 
-
 # 8. Дана строка my_str. Разделите эту строку на пары из двух символов и поместите эти пары в список.
 # Если строка содержит нечетное количество символов, пропущенный второй символ последней пары должен
 # быть заменен подчеркиванием ('_'). Примеры: 'abcd' -> ['ab', 'cd'], 'abcde' -> ['ab', 'cd', e_']
@@ -67,14 +63,12 @@ sub_str = my_str[l_index: r_index]
 my_str = "abcde"
 result = []
 for index, value in enumerate(my_str):
-    # print(index)
     if index % 2 == 0:
-        couple = my_str[index:index+2]
-        if len(couple)>1:
+        couple = my_str[index:index + 2]
+        if len(couple) > 1:
             result.append(couple)
         else:
-            result.append(value+"_")
-
+            result.append(value + "_")
 
 # 9. Дан список чисел. Определите, сколько в этом списке элементов,
 # которые больше суммы двух своих соседей (слева и справа), и НАПЕЧАТАЙТЕ КОЛИЧЕСТВО таких элементов.
@@ -86,26 +80,46 @@ for index in range(1, len(my_list) - 1):
     if my_list[index] > sum([my_list[index - 1], my_list[index + 1]]):
         counter = 1 + counter
 
-
 # 10. Дан список my_list в котором могут быть как строки (type str) так и целые числа (type int).
 # Например [1, 2, 3, "11", "22", 33]
 # Создать новый список в который поместить только строки из my_list.
 my_list = [1, 2, 3, "11", "22", 33]
 result_list = []
 for value in my_list:
-    if isinstance(value,str):
+    if isinstance(value, str):
         result_list.append(value)
-
-
-
 
 #
 # 11. Дана строка my_str. Создать список в который поместить те символы из my_str,
 # которые встречаются в строке ТОЛЬКО ОДИН раз.
+my_str = "trollo"
+my_list = []
+for symbol in my_str:
+    amount = my_str.count(symbol)
+    if amount == 1:
+        my_list.append(symbol)
+
 #
 # 12. Даны две строки. Создать список в который поместить те символы,
 # которые есть в обеих строках хотя бы раз.
+my_str_1 = "qwerty"
+my_str_2 = "qryip"
+my_list = []
+for symb_1 in my_str_1:
+    for symb_2 in my_str_2:
+        if symb_1 == symb_2:
+            my_list.append(symb_1)
+my_list = list(set(my_list))
+
 #
 # 13. Даны две строки. Создать список в который поместить те символы, которые есть в обеих строках,
 # но в каждой ТОЛЬКО ПО ОДНОМУ разу.
 # Пример: для строк "aaaasdf1" и "asdfff2" ответ ["s", "d"], т.к. эти символы есть в каждой строке по одному разу
+my_str1 = "aaaasdf1"
+my_str2 = "asdfff2"
+my_list = []
+new_str1 = ""
+for symb_1 in my_str1:
+    for symb_2 in my_str2:
+        if my_str1.count(symb_1) == 1 and my_str2.count(symb_2) == 1 and symb_1 == symb_2:
+            my_list.append(symb_1)
