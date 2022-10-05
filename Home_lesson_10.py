b2cb56fefafab6a7a6663e1786238843448f8645
666Luciiifer666 <nikita.ylitin@gmail.com>
2022-10-05 23:12:21 +0300

    name

--- Home_lesson_10.py
+++ Home_lesson_10.py
@@ -1,35 +1,38 @@
-
 # Все пункты сделать как отдельные функции и их вызовы.
 #
 # 1. Написать функцию, которая получает в виде параметра имя файла названия интернет доменов (domains.txt)
 # и возвращает их в виде списка строк (названия возвращать без точки).
-import os
 
-filename = C:\Users\user\PycharmProjects\Python_25_08_22\domains.txt
-
-
-with open(filename, 'r') as file:
-    data = [line.strip() for line in file.readlines()]
-    data = file.readlines()
-print(data)
-
-
-
-
-
-
+# file_name = "Files_homeworks/domains.txt"
+# def domain_as_a_list(filename):
 #
-
-
-
-
-
+#     name = open(filename, "r")
+#     text = name.read()
+#     text = text.replace(".", "")
+#     return(text)
+# text = domain_as_a_list(file_name)
+#
+#
 
 # 2. Написать функцию, которая получает в виде параметра имя файла (names.txt)
 # и возвращает список всех фамилий из него.
 # Каждая строка файла содержит номер, фамилию, страну, некоторое число (таблица взята с википедии).
 # Разделитель - символ табуляции "\t"
-#
+
+file_name = "Files_homeworks/names.txt"
+
+def read_only_names (file_name):
+    file = open(file_name, 'r')
+    data = file.read()
+    print(data)
+    data = data.split('\t')[1::3]
+    return data
+
+data = read_only_names(file_name)
+print(data)
+print(file_name)
+
+
 # 3. Написать функцию, которая получает в виде параметра имя файла (authors.txt) и возвращает список
 # словарей вида {"date": date}
 # в которых date - это дата из строки (если есть),
