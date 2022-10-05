a0b0f0cfdc139a323afacfa059d31557470a8cb5
666Luciiifer666 <nikita.ylitin@gmail.com>
2022-10-06 01:26:20 +0300

    Homeworks lesson 10

--- Home_lesson_10.py
+++ Home_lesson_10.py
@@ -1,5 +1,4 @@
 # Все пункты сделать как отдельные функции и их вызовы.
-#
 # 1. Написать функцию, которая получает в виде параметра имя файла названия интернет доменов (domains.txt)
 # и возвращает их в виде списка строк (названия возвращать без точки).
 
@@ -37,5 +36,22 @@ text = read_only_names(file_name)
 # Например [{"date": "1st January 1919"}, {"date": "8th February 1828"},  ...]
 
 
+input = "Files_homeworks/authors.txt"
 
 
+def parse_dates(input_file):
+    result = list()
+    file = open(input_file, "r")
+    file = file.read()
+    read_file = file.splitlines()
+    for line in read_file:
+        if line != "":
+            info = line.split('-')
+            dates = info[0]
+
+            if len(dates.split()) > 2:
+                result.append({'date': dates})
+    return result
+
+
+test = parse_dates(input)
