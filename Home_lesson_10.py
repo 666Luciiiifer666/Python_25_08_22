31d71322a249b768b81a4eec90c9f22c24e4a8c9
666Luciiifer666 <nikita.ylitin@gmail.com>
2022-10-05 23:47:50 +0300

    Homeworks lesson 10

--- Home_lesson_10.py
+++ Home_lesson_10.py
@@ -3,16 +3,15 @@
 # 1. Написать функцию, которая получает в виде параметра имя файла названия интернет доменов (domains.txt)
 # и возвращает их в виде списка строк (названия возвращать без точки).
 
-# file_name = "Files_homeworks/domains.txt"
-# def domain_as_a_list(filename):
-#
-#     name = open(filename, "r")
-#     text = name.read()
-#     text = text.replace(".", "")
-#     return(text)
-# text = domain_as_a_list(file_name)
-#
-#
+file_name = "Files_homeworks/domains.txt"
+def domain_as_a_list(filename):
+    name = open(filename, "r")
+    text = name.read()
+    text = text.replace(".", "")
+    return(text)
+text = domain_as_a_list(file_name)
+
+
 
 # 2. Написать функцию, которая получает в виде параметра имя файла (names.txt)
 # и возвращает список всех фамилий из него.
@@ -21,27 +20,22 @@
 
 file_name = "Files_homeworks/names.txt"
 
-def read_only_names (file_name):
+def read_only_names(file_name):
     file = open(file_name, 'r')
-    data = file.read()
-    print(data)
-    data = data.split('\t')[1::3]
-    return data
+    text = file.read()
+    text = text.split('\t')[1::3]
+    return text
 
-data = read_only_names(file_name)
-print(data)
-print(file_name)
+text = read_only_names(file_name)
+
+
 
 
 # 3. Написать функцию, которая получает в виде параметра имя файла (authors.txt) и возвращает список
 # словарей вида {"date": date}
 # в которых date - это дата из строки (если есть),
 # Например [{"date": "1st January 1919"}, {"date": "8th February 1828"},  ...]
-#
-# По просьбам некоторых студентов начну включать дополнительные задания.
-# 4* (*сдавать не обязательно, но если будете сдавать, то ошибки будут учитываться тоже).
-# Написать функцию, которая получает в виде параметра имя файла (authors.txt) и возвращает список
-# словарей вида {"date_original": date_original, "date_modified": date_modified}
-# в которых date_original - это дата из строки (если есть),
-# а date_modified - это та же дата, представленная в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
-# Например [{"date_original": "8th February 1828", "date_modified": 08/02/1828},  ...]
+
+
+
+
